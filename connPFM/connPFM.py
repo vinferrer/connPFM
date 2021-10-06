6e3c0263a4a58406cbd5afb16398d4f1e21b3faf
vinferrer <v.ferrer@bcbl.eu>
2021-10-06 17:34:26 +0200

    update dummy workflow to work with print

--- connPFM/connPFM.py
+++ connPFM/connPFM.py
@@ -1,10 +1,12 @@
 import logging
+import sys
 
 LGR = logging.getLogger(__name__)
+LGR.setLevel(logging.INFO)
 
 def _main(argv=None):
-    LGR.info("ConnPFM is in development, we will implement main workflow in future releases")
+    print("ConnPFM is in development, we will implement main workflow in future releases")
 
 
 if __name__ == "__main__":
-    _main()
+    _main(sys.argv[1:])

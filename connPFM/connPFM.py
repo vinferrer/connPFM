d34e7525f5fe6fbcc899d2731e5372976818234c
vferrer <vferrer@bcbl.eu>
2021-10-06 17:14:44 +0200

    add dummy main

---
+++ connPFM/connPFM.py
@@ -0,0 +1,10 @@
+import logging
+
+LGR = logging.getLogger(__name__)
+
+def _main(argv=None):
+    LGR.info("ConnPFM is in development, we will implement main workflow in future releases")
+
+
+if __name__ == "__main__":
+    _main()

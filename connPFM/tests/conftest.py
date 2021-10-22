53a95dbe7207c590bf23864225ec153ec11c0a1b
vinferrer <v.ferrer@bcbl.eu>
2021-10-22 13:40:45 +0200

    apply black and add more fixtures

--- connPFM/tests/conftest.py
+++ connPFM/tests/conftest.py
@@ -7,13 +7,14 @@ import pytest
 
 
 def pytest_addoption(parser):
-    parser.addoption('--skipintegration', action='store_true',
-                     default=False, help='Skip integration tests.')
+    parser.addoption(
+        "--skipintegration", action="store_true", default=False, help="Skip integration tests."
+    )
 
 
 @pytest.fixture
 def skip_integration(request):
-    return request.config.getoption('--skipintegration')
+    return request.config.getoption("--skipintegration")
 
 
 def fetch_file(osf_id, path, filename):
@@ -37,42 +38,64 @@ def fetch_file(osf_id, path, filename):
     """
     # This restores the same behavior as before.
     # this three lines make tests dowloads work in windows
-    if os.name == 'nt':
+    if os.name == "nt":
         orig_sslsocket_init = ssl.SSLSocket.__init__
-        ssl.SSLSocket.__init__ = lambda *args, cert_reqs=ssl.CERT_NONE, **kwargs: orig_sslsocket_init(*args, cert_reqs=ssl.CERT_NONE, **kwargs)
+        ssl.SSLSocket.__init__ = (
+            lambda *args, cert_reqs=ssl.CERT_NONE, **kwargs: orig_sslsocket_init(
+                *args, cert_reqs=ssl.CERT_NONE, **kwargs
+            )
+        )
         ssl._create_default_https_context = ssl._create_unverified_context
-    url = 'https://osf.io/{}/download'.format(osf_id)
+    url = "https://osf.io/{}/download".format(osf_id)
     full_path = os.path.join(path, filename)
     if not os.path.isfile(full_path):
         urlretrieve(url, full_path)
     return full_path
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def testpath(tmp_path_factory):
-    """ Test path that will be used to download all files """
+    """Test path that will be used to download all files"""
     return tmp_path_factory.getbasetemp()
+
 
 @pytest.fixture
 def bold_file(testpath):
-    return fetch_file('x6r8v', testpath,
-                      'sub-pixar123_task-pixar_space-MNI152-preproc_bold.nii.gz')
+    return fetch_file(
+        "x6r8v", testpath, "sub-pixar123_task-pixar_space-MNI152-preproc_bold.nii.gz"
+    )
+
 
 @pytest.fixture
 def AUC_file(testpath):
-    return fetch_file('h6uv3', testpath,
-                      'sub-pixar123_task-pixar_AUC_100.nii.gz')
+    return fetch_file("h6uv3", testpath, "sub-pixar123_task-pixar_AUC_100.nii.gz")
+
 
 @pytest.fixture
-def ets_matrix_file(testpath):
-    return fetch_file('jvuwn', testpath,
-                      'ets_AUC_denoised.txt')
+def ets_auc_original_file(testpath):
+    return fetch_file("bnp4z", testpath, "ets_AUC_original.txt")
+
+
+@pytest.fixture
+def ets_auc_denoised_file(testpath):
+    return fetch_file("jvuwn", testpath, "ets_AUC_denoised.txt")
+
 
 @pytest.fixture
 def surr_dir(testpath):
-    zipped_file = fetch_file('yrkqv', testpath,
-                             'temp_sub-pixar123_100.tar.gz')
+    zipped_file = fetch_file("yrkqv", testpath, "temp_sub-pixar123_100.tar.gz")
     my_tar = tarfile.open(zipped_file)
-    my_tar.extractall(testpath) # specify which folder to extract to
+    my_tar.extractall(testpath)  # specify which folder to extract to
     my_tar.close()
-    return os.path.join(testpath,'temp_sub-pixar123_100')
+    return os.path.join(testpath, "temp_sub-pixar123_100")
+
+
+@pytest.fixture
+def atlas_file(testpath):
+    return fetch_file("bhcrp", testpath,
+                      "Schaefer2018_100Parcels_17Networks_order_FSLMNI152_4mm.nii.gz")
+
+
+@pytest.fixture
+def rssr_auc_file(testpath):
+    return fetch_file("7pnj2", testpath, "rssr_AUC.txt")

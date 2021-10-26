fcd254d82eb6c76ad000167e6b81596c5fcfaac3
vinferrer <v.ferrer@bcbl.eu>
2021-10-26 17:20:33 +0200

    add utils testing file

---
+++ connPFM/tests/test_utils.py
@@ -0,0 +1,25 @@
+from os.path import join
+
+import numpy as np
+
+from nilearn.input_data import NiftiLabelsMasker
+
+
+def test_surrogate_generator(bold_file, atlas_file, testpath):
+    masker = NiftiLabelsMasker(
+        labels_img=atlas_file,
+        standardize=False,
+        memory=None,
+        strategy="mean",
+        resampling_target=None,
+    )
+    np.random.seed(200)
+
+    from connPFM.utils import surrogate_generator
+    surrogate = surrogate_generator.generate_surrogate(bold_file,
+                                                       atlas_file,
+                                                       join(testpath,
+                                                            'generated_surrogate.nii.gz'))
+    keeped_surrogate = masker.fit_transform('/home/vicente/nilearn_data/random_200_surrogate.nii.gz')
+    breakpoint()
+    assert np.all(np.isclose(surrogate, keeped_surrogate))

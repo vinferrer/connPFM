69ee2e668b08fa72015be649dad624678b235a57
vinferrer <v.ferrer@bcbl.eu>
2021-11-01 20:11:21 +0100

    add hrf_loaded to the assert

--- connPFM/tests/test_utils.py
+++ connPFM/tests/test_utils.py
@@ -29,7 +29,7 @@ def test_HRF_matrix(hrf_file, hrf_linear_file):
     hrf_object = hrf_generator.HRFMatrix(TR=1, TE=[0], nscans=168)
     hrf_object.generate_hrf()
     hrf_loaded = np.loadtxt(hrf_file)
-    assert np.all(np.isclose(hrf_object.hrf))
+    assert np.all(np.isclose(hrf_object.hrf,hrf_loaded))
     hrf_linear = hrf_generator.HRFMatrix(TR=1, TE=[0], is_afni=False, nscans=168)
     hrf_linear.generate_hrf()
     assert np.all(np.isclose(hrf_linear.hrf, np.loadtxt(hrf_linear_file)))

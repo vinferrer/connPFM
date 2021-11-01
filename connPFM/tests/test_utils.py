51366e24d899912c3e723cce2b2f6efcfc4776b3
vinferrer <v.ferrer@bcbl.eu>
2021-11-01 20:03:33 +0100

    run black

--- connPFM/tests/test_utils.py
+++ connPFM/tests/test_utils.py
@@ -35,5 +35,6 @@ def test_HRF_matrix(hrf_file, hrf_linear_file):
     assert np.all(np.isclose(hrf_linear.hrf, np.loadtxt(hrf_linear_file)))
     hrf_block = hrf_generator.HRFMatrix(TR=1, TE=[0], nscans=168, block=True)
     hrf_block.generate_hrf()
-    assert np.all(np.isclose(hrf_block.hrf, np.matmul(hrf_loaded,
-                             np.tril(np.ones(hrf_loaded.shape[0])))))
+    assert np.all(
+        np.isclose(hrf_block.hrf, np.matmul(hrf_loaded, np.tril(np.ones(hrf_loaded.shape[0]))))
+    )

97e76cde8e2349f2bae8d72ae55345a1a42fa7d3
vferrer <vferrer@bcbl.eu>
2021-11-05 16:25:31 +0100

    change colormap and setup vmin and vmax defaults

--- connPFM/connectivity/plotting.py
+++ connPFM/connectivity/plotting.py
@@ -30,11 +30,15 @@ LGR = logging.getLogger(__name__)
 
 
 def plot_ets_matrix(
-    ets, outdir, sufix="", dvars_file=None, enorm_file=None, peaks=None, vmin=-0.5, vmax=0.5
+    ets, outdir, sufix="", dvars_file=None, enorm_file=None, peaks=None, vmin=None, vmax=None
 ):
     """
     Plots edge-time matrix
     """
+    if vmin == None:
+            vmin = np.min(ets)
+    if vmax == None:
+            vmax = np.max(ets)/2
     if dvars_file is not None and enorm_file is not None:
         # Plot ETS matrix of original signal
         dvars = np.loadtxt(dvars_file)
@@ -58,7 +62,7 @@ def plot_ets_matrix(
         ax1 = divider.append_axes("bottom", size="25%", pad=1)
         ax2 = divider.append_axes("bottom", size="25%", pad=1)
         cax = divider.append_axes("right", size="5%", pad=0.08)
-        im = ax0.imshow(ets.T, vmin=vmin, vmax=vmax, cmap="bwr", aspect="auto")
+        im = ax0.imshow(ets.T, vmin=vmin, vmax=vmax, cmap="OrRd", aspect="auto")
         ax0.set_ylabel("Edge-edge connections")
         plt.colorbar(im, orientation="vertical", ax=ax0, cax=cax)  # ax=axs.ravel().tolist()
         dvars[1] = np.mean(dvars)
@@ -66,16 +70,16 @@ def plot_ets_matrix(
         ax1.set_title("DVARS")
         ax1.margins(0, 0)
         for i in peaks:
-            ax1.axvspan(i, i + 1, facecolor="b", alpha=0.5)
-            ax2.axvspan(i, i + 1, facecolor="b", alpha=0.5)
+            ax1.axvspan(i, i + 1, facecolor="g", alpha=0.5)
+            ax2.axvspan(i, i + 1, facecolor="g", alpha=0.5)
         ax2.plot(enorm)
         ax2.set_title("ENORM")
         ax2.set_xlabel("Time (TR)")
         ax2.margins(0, 0)
         plt.savefig(opj(outdir, f"ets{sufix}.png"), dpi=300)
     else:
-        fig, axs = plt.subplots(1, 1, figsize=FIGSIZE)
-        plt.imshow(ets.T, vmin=vmin, vmax=vmax, cmap="bwr", aspect="auto")
+        fig, _ = plt.subplots(1, 1, figsize=FIGSIZE)
+        plt.imshow(ets.T, vmin=vmin, vmax=vmax, cmap="OrRd", aspect="auto")
         plt.title("Edge-time series")
         plt.xlabel("Time (TR)")
         plt.ylabel("Edge-edge connections")

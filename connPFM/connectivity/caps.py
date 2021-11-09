61560865c8b00e0b3bf9616be4efeb40f734c475
vinferrer <v.ferrer@bcbl.eu>
2021-11-09 15:24:38 +0100

    add option to load surrogates instead of circle shift and add masker for PCC mask with option to psc or not

--- connPFM/connectivity/caps.py
+++ connPFM/connectivity/caps.py
@@ -2,7 +2,7 @@ import sys
 
 import numpy as np
 from joblib import Parallel, delayed
-from nilearn.input_data import NiftiLabelsMasker
+from nilearn.input_data import NiftiLabelsMasker, NiftiMasker
 from nilearn.masking import apply_mask
 
 from connPFM.cli.caps import _get_parser
@@ -14,29 +14,34 @@ from connPFM.connectivity.ev import (
 from connPFM.utils import atlas_mod
 
 
-def read_data(path, mask, atlas=True):
+def read_data(path, mask, atlas=True,standard="psc"):
     """
     Reads data from a given path and mask or atlas.
     """
     if atlas:
         masker = NiftiLabelsMasker(
             labels_img=mask,
-            standardize=False,
+            standardize=standard,
             memory="nilearn_cache",
             strategy="mean",
         )
         data = masker.fit_transform(path)
+        return data, masker
     else:
-        masker = None
-        data = apply_mask(path, mask)
-    return data, masker
+        masker = NiftiMasker(
+            mask_img=mask,
+            standardize=standard,
+            memory="nilearn_cache",
+        )
+        data = masker.fit_transform(path)
+        return data
 
 
-def caps(data_path, atlas, mask, output, nsur=100, njobs=-1):
+def caps(data_path, atlas, mask, output,surrprefix =None,surrsufix = "", nsur=100, njobs=-1):
     # Read original data and data inside ROI
     print("Reading data...")
     data, masker = read_data(data_path, atlas)
-    roi_data, _ = read_data(data_path, mask, atlas=False)
+    roi_data = read_data(data_path, mask, atlas=False)
 
     # Get number of time points/nodes
     [t, n] = roi_data.shape
@@ -49,11 +54,17 @@ def caps(data_path, atlas, mask, output, nsur=100, njobs=-1):
     rssr = np.zeros((t, nsur))
 
     # Generate surrogates and calculare RSS on them
-    print("Generating surrogates of ROI data...")
-    results = Parallel(n_jobs=njobs, backend="multiprocessing")(
-        delayed(circular_shift_randomization)(roi_data, n, t) for irand in range(nsur)
-    )
-
+    if surrprefix is None:
+        print("Generating surrogates of ROI data...")
+        results = Parallel(n_jobs=njobs, backend="multiprocessing")(
+            delayed(circular_shift_randomization)(roi_data, n, t) for irand in range(nsur)
+        )
+    else:
+        print("Reading surrogates...")
+        results = Parallel(n_jobs=njobs, backend="multiprocessing")(
+            delayed(read_data)(surrprefix + str(irand) + surrsufix + ".nii.gz",mask,False,False) for irand in range(nsur)
+        )
+    
     # Calculate RSS of surrogate data
     print("Calculating RSS of surrogate data...")
     for irand in range(nsur):
@@ -91,6 +102,7 @@ def _main(argv=None):
         atlas=options["atlas"],
         output=options["output"],
         mask=options["mask"],
+        surrprefix=options["surrprefix"],
         nsur=options["nsur"],
         njobs=options["njobs"],
     )

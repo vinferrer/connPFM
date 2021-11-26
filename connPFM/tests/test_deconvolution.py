46ca2a7a4050e48d5d21156dbe76200b3c9ec1f7
vinferrer <v.ferrer@bcbl.eu>
2021-11-26 14:34:13 +0100

    apply black

--- connPFM/tests/test_deconvolution.py
+++ connPFM/tests/test_deconvolution.py
@@ -11,8 +11,8 @@ def test_stability_lars(surr_dir):
     # Load data
     data_filename = join(surr_dir, "data.npy")
     data = np.load(data_filename)
-    first=0
-    last=100
+    first = 0
+    last = 100
     if first is None:
         nvoxels = 1
     else:
@@ -25,19 +25,19 @@ def test_stability_lars(surr_dir):
 
     # Calculates number of TE
     nTE = int(hrf.shape[0] / data.shape[0])
-    nvoxels=data.shape[1]
-    nscans=data.shape[0]
+    nvoxels = data.shape[1]
+    nscans = data.shape[0]
     auc = np.zeros((nscans, nvoxels))
     np.random.seed(200)
     from connPFM.deconvolution.stability_lars import StabilityLars
+
     sl = StabilityLars()
     for vox_idx in range(nvoxels):
         sl.stability_lars(hrf, np.expand_dims(data[:, voxel + vox_idx], axis=-1))
         auc[:, vox_idx] = np.squeeze(sl.auc)
 
-    
     # load saved AUC
     auc_filename = join(surr_dir, "auc_0_OSF.npy")
     auc_osf = np.load(auc_filename)
     # Check if AUC is correct
-    assert np.all(auc==auc_osf)
+    assert np.all(auc == auc_osf)

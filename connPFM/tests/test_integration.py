861c82ec1c162a2e2aeeddab2e2705a7612ed203
vinferrer <v.ferrer@bcbl.eu>
2021-11-27 11:45:24 +0100

    add integration debiasing

--- connPFM/tests/test_integration.py
+++ connPFM/tests/test_integration.py
@@ -1,4 +1,4 @@
-from os.path import join
+from os.path import join, basename
 
 import random 
 import numpy as np
@@ -7,7 +7,7 @@ import pytest
 import subprocess
 from nilearn.input_data import NiftiLabelsMasker
 
-@pytest.mark.integration_pfm
+
 def test_integration_pfm(testpath,bold_file,atlas_file,AUC_file):
     auc_output=join(testpath,'auc_local.nii.gz')
     subprocess.call("export mode=integration_pfm && connPFM -i {} -a {} --AUC {} -tr 1 -u vferrer -job 0 -nsur 1 -w pfm".format(bold_file,atlas_file,auc_output),shell=True)
@@ -23,10 +23,24 @@ def test_integration_pfm(testpath,bold_file,atlas_file,AUC_file):
     np.all(auc_osf==auc_local)
 
 
-@pytest.mark.integration_ev
 def test_integration_ev(testpath,bold_file,atlas_file,AUC_file,ets_auc_denoised_file,surr_dir):
     subprocess.call("connPFM -i {} -a {} --AUC {} -d {} -m {} -tr 1 -u vferrer -nsur 50 -w ev".format(bold_file,atlas_file,AUC_file,surr_dir,join(testpath,'ets_AUC_denoised.txt')),shell=True)
     ets_auc_denoised_local=np.loadtxt(join(testpath,'ets_AUC_denoised.txt'))
     ets_auc_osf=np.loadtxt(join(ets_auc_denoised_file))
     np.all(ets_auc_denoised_local==ets_auc_osf)
+
+
+def test_integration_debias(testpath,bold_file,atlas_file,AUC_file,ets_auc_denoised_file,surr_dir,beta_file,fitt_file):
+    subprocess.call("connPFM -i {} -a {} --AUC {} -d {} -m {} -tr 1 -u vferrer -nsur 50 -w debias".format(bold_file,atlas_file,AUC_file,surr_dir,ets_auc_denoised_file),shell=True)
+    masker = NiftiLabelsMasker(
+        labels_img=atlas_file,
+        standardize=False,
+        strategy="mean",
+    )
+    beta_osf=masker.fit_transform(beta_file)
+    fitt_osf=masker.fit_transform(fitt_file)
+    beta_local=masker.fit_transform(join(testpath, f"{basename(bold_file[:-7])}_beta_ETS.nii.gz"))
+    fitt_local=masker.fit_transform(join(testpath, f"{basename(bold_file[:-7])}_fitt_ETS.nii.gz"))
+    np.all(beta_osf==beta_local)
+    np.all(fitt_osf==fitt_local)
 

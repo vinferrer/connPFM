77074c4bd07a89284b97945157d8b6619218ffff
vinferrer <v.ferrer@bcbl.eu>
2021-11-27 11:53:20 +0100

    add flag to skip integration in unittest

--- connPFM/tests/test_integration.py
+++ connPFM/tests/test_integration.py
@@ -8,7 +8,9 @@ import subprocess
 from nilearn.input_data import NiftiLabelsMasker
 
 
-def test_integration_pfm(testpath,bold_file,atlas_file,AUC_file):
+def test_integration_pfm(testpath,bold_file,atlas_file,AUC_file,skip_integration):
+    if skip_integration:
+        pytest.skip("Skipping integration test")
     auc_output=join(testpath,'auc_local.nii.gz')
     subprocess.call("export mode=integration_pfm && connPFM -i {} -a {} --AUC {} -tr 1 -u vferrer -job 0 -nsur 1 -w pfm".format(bold_file,atlas_file,auc_output),shell=True)
     masker = NiftiLabelsMasker(
@@ -23,14 +25,18 @@ def test_integration_pfm(testpath,bold_file,atlas_file,AUC_file):
     np.all(auc_osf==auc_local)
 
 
-def test_integration_ev(testpath,bold_file,atlas_file,AUC_file,ets_auc_denoised_file,surr_dir):
-    subprocess.call("connPFM -i {} -a {} --AUC {} -d {} -m {} -tr 1 -u vferrer -nsur 50 -w ev".format(bold_file,atlas_file,AUC_file,surr_dir,join(testpath,'ets_AUC_denoised.txt')),shell=True)
+def test_integration_ev(testpath,bold_file,atlas_file,AUC_file,ets_auc_denoised_file,surr_dir,skip_integration):
+    if skip_integration:
+        pytest.skip("Skipping integration test")s
+    ubprocess.call("connPFM -i {} -a {} --AUC {} -d {} -m {} -tr 1 -u vferrer -nsur 50 -w ev".format(bold_file,atlas_file,AUC_file,surr_dir,join(testpath,'ets_AUC_denoised.txt')),shell=True)
     ets_auc_denoised_local=np.loadtxt(join(testpath,'ets_AUC_denoised.txt'))
     ets_auc_osf=np.loadtxt(join(ets_auc_denoised_file))
     np.all(ets_auc_denoised_local==ets_auc_osf)
 
 
-def test_integration_debias(testpath,bold_file,atlas_file,AUC_file,ets_auc_denoised_file,surr_dir,beta_file,fitt_file):
+def test_integration_debias(testpath,bold_file,atlas_file,AUC_file,ets_auc_denoised_file,surr_dir,beta_file,fitt_file,skip_integration):
+    if skip_integration:
+        pytest.skip("Skipping integration test")
     subprocess.call("connPFM -i {} -a {} --AUC {} -d {} -m {} -tr 1 -u vferrer -nsur 50 -w debias".format(bold_file,atlas_file,AUC_file,surr_dir,ets_auc_denoised_file),shell=True)
     masker = NiftiLabelsMasker(
         labels_img=atlas_file,

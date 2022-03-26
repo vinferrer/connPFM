4ea1ca29fa120dd2633bbc83b6640d5de5a93ba5
vinferrer <v.ferrer@bcbl.eu>
2022-03-26 14:00:17 +0100

    add docstring in connectivity_utils.py

--- connPFM/connectivity/connectivity_utils.py
+++ connPFM/connectivity/connectivity_utils.py
@@ -8,7 +8,24 @@ LGR = logging.getLogger(__name__)
 
 
 def calculate_ets(y, n):
-    """Calculate edge-time series."""
+    """
+    Calculate edge-time series.
+    
+    Parameters
+    ----------
+    y : numpy matrix
+        matrix with time-series for each ROI
+    n : list
+        number of nodes
+    Returns
+    -------
+    u : vector of indices for the upper triangle of the matrix y axis
+        
+    v : vector of indices for the upper triangle of the matrix x axis
+
+    ets: matrix
+        edge time-series
+    """
     # upper triangle indices (node pairs = edges)
     u, v = np.argwhere(np.triu(np.ones(n), 1)).T
 
@@ -19,7 +36,39 @@ def calculate_ets(y, n):
 
 
 def rss_surr(z_ts, u, v, surrprefix, sursufix, masker, irand, nbins, hist_range=(0, 1)):
-    """Calculate RSS on surrogate data."""
+    """
+    Calculate RSS on surrogate data.
+
+    Parameters
+    ----------
+    z_ts : numpy matrix
+        z-scored time-series matrix fore each ROI
+    u : vector of indices for the upper triangle of the matrix y axis
+        
+    v : vector of indices for the upper triangle of the matrix x axis
+    surrprefix : string
+        prefix of the surrogate file
+    sursufix : string
+        suffix of the surrogate file
+    masker : instance of NiftiMasker
+        masker object to load the surrogate data
+    irand : int
+        index of the surrogate
+    nbins : int
+        number of bins for the histogram
+    hist_range : tuple
+        range of the histogram
+    Returns
+    -------
+    rssr : numpy matrix
+        RSSr matrix
+    etsr : numpy matrix
+        edge-time series matrix for surrogate
+    ets_hist : numpy matrix
+        edge-time series histogram for surrogate
+    bin_edges : numpy matrix
+        histogram bins for the edge-time series matrix
+    """
     [t, n] = z_ts.shape
 
     if surrprefix != "":
@@ -83,6 +132,19 @@ def threshold_ets_matrix(ets_matrix, thr, selected_idxs=None):
     """
     Threshold the edge time-series matrix based on the selected time-points and
     the surrogate matrices.
+    
+    Parameters
+    ----------
+    ets_matrix : numpy matrix
+        edge time-series matrix
+    thr : float
+        threshold value
+    selected_idxs : numpy array
+        indices of the selected time-points
+    Returns
+    -------
+    thresholded_matrix : numpy matrix
+        thresholded edge time-series matrix
     """
     # Initialize matrix with zeros
     thresholded_matrix = np.zeros(ets_matrix.shape)
@@ -106,7 +168,23 @@ def threshold_ets_matrix(ets_matrix, thr, selected_idxs=None):
 
 
 def calculate_surrogate_ets(surrprefix, sursufix, irand, masker):
-    """Read surrogate data."""
+    """
+    Read surrogate data.
+
+    Parameters
+    ----------
+    surrprefix : string
+        prefix of the surrogate file
+    sursufix : string
+        suffix of the surrogate file
+    irand : int
+        index of the surrogate
+    masker : instance of NiftiMasker
+    Returns
+    -------
+    ets : numpy matrix
+        edge time-series matrix
+    """
     auc = masker.fit_transform(f"{surrprefix}{irand}{sursufix}.nii.gz")
     [t, n] = auc.shape
     ets, _, _ = calculate_ets(np.nan_to_num(auc), n)
@@ -122,7 +200,28 @@ def calculate_hist(
     hist_range,
     nbins=500,
 ):
-    """Calculate histogram."""
+    """
+    Calculate histogram.
+    Parameters
+    ----------
+    surrprefix : string
+        prefix of the surrogate file
+    sursufix : string
+        suffix of the surrogate file
+    irand : int
+        index of the surrogate
+    masker : instance of NiftiMasker
+    hist_range : tuple
+        range of the histogram
+    nbins : int
+        number of bins for the histogram
+    Returns
+    -------
+    ets_hist : numpy matrix
+        edge-time series histogram for surrogate
+    bin_edges : numpy matrix
+        histogram bins for the edge-time series matrix
+    """
     ets_temp = calculate_surrogate_ets(surrprefix, sursufix, irand, masker)
 
     ets_hist, bin_edges = np.histogram(ets_temp.flatten(), bins=nbins, range=hist_range)
@@ -131,7 +230,21 @@ def calculate_hist(
 
 
 def calculate_hist_threshold(hist, bins, percentile=95):
-    """Calculate histogram threshold."""
+    """
+    Calculate histogram threshold.
+    Parameters
+    ----------
+    hist : numpy matrix
+        edge-time series histogram for surrogate
+    bins : numpy matrix
+        histogram bins for the edge-time series matrix
+    percentile : float
+        percentile for the histogram threshold
+    Returns
+    -------
+    thr : float
+        threshold value
+    """
     cumsum_percentile = np.cumsum(hist) / np.sum(hist) * 100
     thr = bins[len(cumsum_percentile[cumsum_percentile <= percentile])]
 
@@ -144,6 +257,14 @@ def sum_histograms(
     """
     Get histograms of all surrogates and sum them to
     obtain a single histogram that summarizes the data.
+    Parameters
+    ----------
+    hist_list : list
+        list of histograms
+    Returns
+    -------
+    hist_sum : numpy matrix
+        histogram of all surrogates
     """
 
     # Initialize matrix to store surrogate histograms
